da022acbed54f395c77afa858d85d9b9ef552114
LaySent <laysent@gmail.com>
2019-03-02 13:56:35 +0800

    add: unist-util-to-list-of-char helper package

---
+++ packages/unist-util-to-list-of-char/index.test.js
@@ -0,0 +1,77 @@
+const remark = require('remark');
+const toList = require('.');
+
+describe('unist-util-visit-list-of-char', () => {
+  it('should iterate through text', () => {
+    const markdown = 'ok';
+    const tree = remark().parse(markdown);
+    toList(tree, (list) => {
+      expect(list).toEqual([
+        {
+          value: 'o', column: 1, line: 1, offset: 0,
+        },
+        {
+          value: 'k', column: 2, line: 1, offset: 1,
+        },
+      ]);
+    });
+  });
+  it('should iterate through text in nested nodes', () => {
+    const markdown = 'o**[k](url)**';
+    const tree = remark().parse(markdown);
+    toList(tree, (list) => {
+      expect(list).toEqual([
+        {
+          value: 'o', column: 1, line: 1, offset: 0,
+        },
+        {
+          value: 'k', column: 5, line: 1, offset: 4,
+        },
+      ]);
+    });
+  });
+  it('should handle newline', () => {
+    const markdown = [
+      'o',
+      '__k__',
+    ].join('\n');
+    const tree = remark().parse(markdown);
+    toList(tree, (list) => {
+      expect(list).toEqual([
+        {
+          value: 'o', column: 1, line: 1, offset: 0,
+        },
+        {
+          value: '\n', column: 2, line: 1, offset: 1,
+        },
+        {
+          value: 'k', column: 3, line: 2, offset: 4,
+        },
+      ]);
+    });
+  });
+  it('should process only parital of tree when test is provided', () => {
+    const markdown = [
+      '1',
+      '',
+      '2',
+    ].join('\n');
+    const tree = remark().parse(markdown);
+    const result = [];
+    toList(tree, 'paragraph', (list) => {
+      result.push(list);
+    });
+    expect(result).toEqual([
+      [
+        {
+          value: '1', column: 1, line: 1, offset: 0,
+        },
+      ],
+      [
+        {
+          value: '2', column: 1, line: 3, offset: 3,
+        },
+      ],
+    ]);
+  });
+});
