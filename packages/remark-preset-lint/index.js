a634d71c651e29c3fd81fc24baf012c45b08f9bc
LaySent <laysent@gmail.com>
2019-09-29 21:58:44 +0800

    feat: disable table-pipe-alignment rule
    reason:
    CJK has different char wide,
    align based on char length result in chaos visually.

--- packages/remark-preset-lint/index.js
+++ packages/remark-preset-lint/index.js
@@ -58,7 +58,6 @@ module.exports.plugins = [
   [require('remark-lint-rule-style'), '---'],
   [require('remark-lint-strong-marker'), '*'],
   [require('remark-lint-table-cell-padding'), 'padded'],
-  require('remark-lint-table-pipe-alignment'),
   require('remark-lint-table-pipes'),
   [require('remark-lint-unordered-list-marker-style'), '+'],
   require('remark-lint-match-punctuation'),
